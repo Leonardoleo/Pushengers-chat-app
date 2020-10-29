ef4f072004a167b1c9448d176e347699da067c5d
Mo Leo Sam <leo89_my@hotmail.com>
2020-10-29 18:32:21 +0800

    adjust button at Linkelement

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { Link } from 'react-router-dom';
-import Background from '../assets/images/online-chat-room.jpg';
+// import Background from '../assets/images/online-chat-room.jpg';
 export default class HomePage extends Component {
     render() {
         return (
             <div className="home">
                <Header></Header>
                <section>
-                   <div className="jumbotron jumbotron-fluid py-5" style={{ background: `url(${Background})`}}>
+                   <div className="jumbotron jumbotron-fluid py-5">
                       <div className="container text-center py-5">
                         <h1 className="display-4">Welcome to Pushengers</h1>
                         <p className="lead">A great place to talk with friends!</p>
                         <div className="mt-4">
-                           <Link type="button" className="btn btn-outline-danger px-5 mr-3" to="/signup">Create New Account</Link>
-                           <Link type="button" className="btn btn-outline-primary px-5 mr-3" to="/login">Login to your account</Link>
+                           <Link className="btn btn-danger btn-lg active text-white px-6 mr-3" type="button" to="/signup">Create New Account</Link>
+                           <Link className="btn btn-primary btn-lg active text-white px-6 mr-3" type="button" to="/login">Login to your account</Link>
                         </div>
                       </div>
                    </div>
@@ -26,3 +26,4 @@ export default class HomePage extends Component {
     }
 }
 
+// style={{ background: `url(${Background})`}}
