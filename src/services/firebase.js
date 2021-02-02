4a1308c743eb7f2da6b03e4f092ddbdfbcf50d3c
LeonardLeo <leo89_my@hotmail.com>
2021-02-02 13:09:57 +0800

    import firebase storageand add the SDK firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,12 +1,17 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
+import "firebase/storage";
 
 const config = {
     apiKey: "AIzaSyAKpLlbMuhvwZz_gc9pyvsHGPouBaduX28",
     authDomain: "pushengers.firebaseapp.com",
     databaseURL: "https://pushengers.firebaseio.com",
-    storageBucket: "pushengers.appspot.com"
+    projectId: "pushengers",
+    storageBucket: "pushengers.appspot.com",
+    messagingSenderId: "234937355452",
+    appId: "1:234937355452:web:0a6d8989ddc91ee3a9d4b9",
+    measurementId: "G-06F0FJ48XJ"
 };
 
 firebase.initializeApp(config);
